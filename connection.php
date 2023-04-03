1091b1afba87a14c1f2fe5aee781bee71dd06ab7
Emil <e.garaev.dg@gmail.com>
2023-04-03 21:42:18 +0300

    com

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+    $db = "lb_pdo_rent";
+    $dsn = "mysql:host=localhost";
+    $user = "root";
+    $pass = "";
+    $dbh = new PDO($dsn, $user, $pass);
+    
+?>
---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+ include('connection.php');
+?>
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <title>ЛБ1</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    
+<h2>Лабораторная работа №1</h2>
+
+<div class="wrapper1">
+    <form action="profit.php" method="get">
+        <p><strong>Выберите дату чтоб узнать доход с проката</strong>
+
+            <input name="selected_date" type=date>
+            <input class="buttons"  type="submit" value="Узнать" />
+
+    </form>
+
+
+    <form method="get" action="vendor.php">
+        <p><strong>Выберите производителя, для просмотра его машин</strong>
+
+            <select name='vend'>
+                <option>Производители</option>
+
+                <?php
+        $sql = "SELECT * FROM lb_pdo_rent.vendors";
+
+        foreach($dbh->query($sql) as $row)
+                {
+                $name_vend = $row['Name'];
+                echo "<option value = '$name_vend'> $name_vend</option>";
+                }
+                ?>
+        </p>
+        </select>
+
+        <input class="buttons" type="submit" value="Посмотреть">
+    </form>
+    <form method="get" action="avaible_car.php">
+        <p><strong>Выберите дату чтоб получить доступные авто</strong>
+
+            <input name="selected_date" type=date >
+            <input class="buttons" type="submit" value="Получить" />
+        </p>
+    </form>
+</div>
+
+
+</body>
+
+</html>
